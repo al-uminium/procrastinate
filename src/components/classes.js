32d12e16f2889954d6490d874988abaefc7e9f08
al-uminium <79945949+al-uminium@users.noreply.github.com>
2021-06-22 19:24:53 +0800

    added lodash, classes and logic

---
+++ src/components/classes.js
@@ -0,0 +1,34 @@
+const inbox = () => { 
+    //stores all projects, but will initialize a separate inbox project as the default project
+    let allProj = [];
+
+    const getAllTasks = () => {
+        allTasks = []
+        for (const proj of allProj) {
+            proj.tasks.map(task => allTasks.push(task))
+        }
+        return allTasks
+    }
+
+    return {allProj, getAllTasks}
+}
+
+const projects = (projName='') => {
+
+    let name = projName
+    let tasks = []
+
+    return {name, tasks}
+}
+
+const task = (taskTitle="", taskDescr="", taskDueDate=null, completeStatus=false, taskPriority="None") => {
+    let title = taskTitle;
+    let descr = taskDescr;
+    let dueDate = taskDueDate;
+    let isComplete = completeStatus
+    let priority = taskPriority;
+
+    return { title, descr, dueDate, isComplete, priority }
+}
+
+export { inbox, projects, task }
---
+++ src/components/logic.js
@@ -0,0 +1,42 @@
+import { inbox, projects, task } from './classes.js'
+import _ from 'lodash'
+
+//initialize inbox
+const generalInbox = inbox();
+
+//Create new project
+const Project = (projName) => {
+    const i = projects(projName)
+    if 
+    generalInbox.allProj.push(i)
+    return i
+}
+
+//Initialize default project
+const defaultProj = Project("Inbox")
+
+//Create new task
+const Task = (taskTitle,taskDescr, taskDueDate, taskPriority, completeStatus=false, taskProject=defaultProj) => {
+    const i = task(taskTitle,taskDescr, taskDueDate, completeStatus=false, taskPriority)
+    taskProject.tasks.push(i)
+}
+
+//Delete task 
+const delTask = (targetTask) => {
+    //targetTask is not an actual task object, so will have to recreate it
+
+    let targetTask = task("","","","","") //placeholder for now
+
+    //Find where the task is stored
+    //Operation is O(n), maybe can optimize?
+    for (const proj of generalInbox) {
+
+        // _.filter(proj, (o) => !(_.isEqual(o, targetTask)))
+
+        for (let i = 0; i < proj.length; i++) {
+            if (_.isEqual(proj[i], targetTask)) {
+                proj.splice(i,1)
+            }
+        }
+    }
+}
